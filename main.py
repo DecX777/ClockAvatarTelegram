b9c9b2777e71db94c36469a4dd0ef19fc4d77360
DecX777 <115566471+DecX777@users.noreply.github.com>
2022-10-11 18:07:37 +0300

    TelegramClockAvatar

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import calendar
 from telethon import TelegramClient, sync
 from telethon.tl.functions.photos import UploadProfilePhotoRequest, DeletePhotosRequest
 
-client = TelegramClient("timeavatar", "3135518", "465ed9250d34ff6666a60948fada90ed")
+client = TelegramClient("timeavatar", "api_id", "api_hash")
 client.start()
 
 def draw():
